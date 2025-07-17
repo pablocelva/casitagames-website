b66ee4de4afde9ef3b6a9924d92fd1ba9e6fb79e
Pablo Lambert <pablomartin.lambert@gmail.com>
2025-07-16 20:25:48 -0400

    prueba imgs

--- src/pages/OjitoGame.jsx
+++ src/pages/OjitoGame.jsx
@@ -6,7 +6,7 @@ export default function OjitoGame() {
             <h1>Ojito Game</h1>
             <h3>Ojito Game</h3>
             <p>Ojito Game is a game where you train your perception and memory.</p>
-            <img src=".https://raw.githubusercontent.com/pablocelva/casitagames-website/refs/heads/main/src/assets/img/ojito/ojitoteam_450x200px.png" alt="Ojito Game" className='game-image' />
+            <img src="https://raw.githubusercontent.com/pablocelva/casitagames-website/refs/heads/main/src/assets/img/ojito/ojitoteam_450x200px.png" alt="Ojito Game" className='game-image' />
             {/* <Footer /> */}
         </section>
     )
